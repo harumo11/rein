312753de22afe086b2d0d4aa84fc243336f25253
harumo11 <s_harumo@hotmail.co.jp>
2018-03-13 19:26:23 +0900

    [update] test_QLearn.cpp and CMakeLists.txt

--- test_QLearn.cpp
+++ test_QLearn.cpp
@@ -5,5 +5,10 @@
 int main(int argc, char const* argv[])
 {
 	
+	rein::QLearn agent;
+	agent.initialize(3,3,0);
+	agent.q_table.show();
+	std::cout << agent.q_table.cols() << std::endl;
+	std::cout << agent.q_table.rows() << std::endl;
 	return 0;
 }
